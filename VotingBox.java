75924df85ed01263b83bd45cc4b4341b3ab41dbd
ikennanjoku91 <ikennanjoku91@gmail.com>
2020-09-26 14:41:15 -0500

    initial commit

---
+++ VotingBox.java
@@ -0,0 +1,34 @@
+import stanford.karel.*;
+
+public class VotingBox extends SuperKarel {
+
+	public void run(){
+		
+		while (frontIsClear()) {
+			move();
+			if (noBeepersPresent()) {
+			removeChad();
+			}
+			move();
+			}	
+		
+	 } 
+	private void removeChad() {
+		turnRight();
+		checkPunchCorner();
+		checkPunchCorner();
+		turnLeft();
+		}
+
+	private void checkPunchCorner() {
+		move();
+		while (beepersPresent()) {
+		pickBeeper();
+		}
+		turnAround();
+		move();
+		}
+
+	 
+}
+
